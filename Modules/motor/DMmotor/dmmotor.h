841e5cf64f9f4bebbc4d72fa48d13ac11b0a9aa2
Enhao Zhang <enhao-zhang@outlook.com>
2025-09-25 15:05:55 +0800

    [feat]dm motor pid & other feedback

--- Modules/motor/DMmotor/dmmotor.h
+++ Modules/motor/DMmotor/dmmotor.h
@@ -1,19 +1,20 @@
 #ifndef DMMOTOR_H
 #define DMMOTOR_H
 #include <stdint.h>
+
 #include "bsp_fdcan.h"
 #include "controller.h"
-#include "motor_def.h"
 #include "daemon.h"
+#include "motor_def.h"
 
 #define DM_MOTOR_CNT 6
 
-#define DM_P_MIN  (-12.5f)
-#define DM_P_MAX  12.5f
-#define DM_V_MIN  (-30.0f) // J4310 30.0f
-#define DM_V_MAX  30.0f
-#define DM_T_MIN  (-10.0f) // J4310 10.0f
-#define DM_T_MAX   10.0f
+#define DM_P_MIN (-12.5f)
+#define DM_P_MAX 12.5f
+#define DM_V_MIN (-30.0f)  // J4310 30.0f J8009P 45.0f
+#define DM_V_MAX 30.0f
+#define DM_T_MIN (-10.0f)  // J4310 10.0f J8009P 54.0f
+#define DM_T_MAX 10.0f
 
 typedef struct {
   uint8_t id;
@@ -24,7 +25,8 @@ typedef struct {
   float torque;
   float T_Mos;
   float T_Rotor;
-  int32_t total_round;
+  float total_angle;    // 总角度,注意方向
+  int32_t total_round;  // 总跳变次数,注意方向
 } DM_Motor_Measure_s;
 
 typedef struct {
@@ -33,7 +35,7 @@ typedef struct {
   uint16_t torque_des;
   uint16_t Kp;
   uint16_t Kd;
-} DMMotor_Send_s;
+} DM_Motor_Send_s;
 
 typedef struct {
   DM_Motor_Measure_s measure;
@@ -50,10 +52,10 @@ typedef struct {
 } DMMotorInstance;
 
 typedef enum {
-  DM_CMD_MOTOR_MODE = 0xfc, // 使能,会响应指令
-  DM_CMD_RESET_MODE = 0xfd, // 停止
-  DM_CMD_ZERO_POSITION = 0xfe, // 将当前的位置设置为编码器零位
-  DM_CMD_CLEAR_ERROR = 0xfb // 清除电机过热错误
+  DM_CMD_MOTOR_MODE = 0xfc,     // 使能,会响应指令
+  DM_CMD_RESET_MODE = 0xfd,     // 停止
+  DM_CMD_ZERO_POSITION = 0xfe,  // 将当前的位置设置为编码器零位
+  DM_CMD_CLEAR_ERROR = 0xfb     // 清除电机过热错误
 } DMMotor_Mode_e;
 
 DMMotorInstance* DMMotorInit(Motor_Init_Config_s* config);
@@ -68,5 +70,9 @@ void DMMotorStop(DMMotorInstance* motor);
 
 void DMMotorCaliEncoder(DMMotorInstance* motor);
 
-void DMMotorControlInit();
-#endif // !DMMOTOR
+void DMMotorPIDCal(DMMotorInstance* motor, float pid_ref);
+
+void DMMotorTask(void const* argument);
+
+void DMMotorTaskInit();
+#endif  // !DMMOTOR
