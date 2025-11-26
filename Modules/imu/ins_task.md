455e346ce16fa0c947f6cb3025ab68d03c9b35a9
周泽乐 <zhouzeyue@qq.com>
2025-11-26 23:47:47 +0800

    [feat&comment]1.加了很多ins_task.c和.h的注释。 2.ins_init函数现在调用时将imu init config(修正开发板和车坐标不同）传进去。 3.该参数目前每辆车都改了，都写在robotconfig的gimbal init config里面，因为一般都是gimbal用。 4.把imutask的rtos线程放到insinit时初始化了。

--- Modules/imu/ins_task.md
+++ Modules/imu/ins_task.md
@@ -15,4 +15,27 @@
 ## 模块移植
 
 由于历史遗留问题,IMU模块耦合程度高.后续实现BSP_SPI,将bmi088 middleware删除.仅保留BMI088读取的协议和寄存器定义等,单独实现IMU模块.
+
 > 移植已经完成,请转而使用module/BMI088的模块. 当前文件夹将在beta1.2停止支持, 1.5之后删除. INS_Task届时会被放到algorithm中,以提供对不同IMU的兼容.
+
+## 修正安装误差
+
+使用的是IMU_Param_Correction函数，用于修正IMU安装误差与标度因数误差，即陀螺仪轴和云台轴的安装偏移。
+函数需要传入一个IMU_Init_Config_s* imu_init_config参数和解算出来的float gyro[3], float accel[3]。
+
+IMU_Init_Config_s包括：
+
+```c
+uint8_t flag;      // 参数更新标志位，当参数发生变化时置1
+float scale[3];    // 三轴标度因数修正系数 [0]-X轴 [1]-Y轴 [2]-Z轴
+float Yaw;         // Yaw轴安装偏角修正 单位: °
+float Pitch;       // Pitch轴安装偏角修正 单位: °
+float Roll;        // Roll轴安装偏角修正 单位: °
+```
+
+flag给1，表示参数更新。
+scale为三轴标度因数修正系数，默认为1.0f，表示一比一。scale增加，表示传感器的测量结果比真值要大。
+Yaw、Pitch、Roll为三轴安装偏角修正系数，默认为0.0f，表示无安装偏角。如果存在安装偏角，比如需要按照下图yaw、roll、pitch方向，设置坐标系旋转角度。例如下图需要按yaw轴正向旋转90°，则Yaw设置为90.0f。下图中就是roll转到pitch位置。
+![imu三轴旋转正方向.png](imu%E4%B8%89%E8%BD%B4%E6%97%8B%E8%BD%AC%E6%AD%A3%E6%96%B9%E5%90%91.png)
+
+IMU_Init_Config_s* imu_init_config写在gimbal.h或者chassis.h里。
--- UserApp/os_task.c
+++ UserApp/os_task.c
@@ -29,7 +29,6 @@
 #include "bsp_init.h"
 #include "dmmotor.h"
 
-osThreadId insTaskHandle;
 osThreadId motorTaskHandle;
 osThreadId daemonTaskHandle;
 osThreadId robotTaskHandle;
@@ -49,8 +48,8 @@ void OSTaskInit() {
 
   // 由于是阻塞读取传感器,为姿态解算设置较高优先级,确保以1khz的频率执行
   // 后续修改为读取传感器数据准备好的中断处理
-  osThreadDef(instask, StartINSTASK, osPriorityAboveNormal, 0, 1024);
-  insTaskHandle = osThreadCreate(osThread(instask), NULL);
+  // osThreadDef(instask, StartINSTASK, osPriorityAboveNormal, 0, 1024);
+  // insTaskHandle = osThreadCreate(osThread(instask), NULL);
 
   osThreadDef(motortask, StartMOTORTASK, osPriorityBelowNormal, 0, 256);
   motorTaskHandle = osThreadCreate(osThread(motortask), NULL);
@@ -68,21 +67,21 @@ void OSTaskInit() {
   __enable_irq();
 }
 
-__attribute__((noreturn)) void StartINSTASK(void const *argument) {
-  static float ins_start;
-  static float ins_dt;
-  INS_Init();  // 确保BMI088被正确初始化.
-  LOGINFO("[freeRTOS] INS Task Start");
-  for (;;) {
-    // 1kHz
-    ins_start = DWT_GetTimeline_ms();
-    INS_Task();
-    ins_dt = DWT_GetTimeline_ms() - ins_start;
-    if (ins_dt > 1) LOGERROR("[freeRTOS] INS Task is being DELAY! dt = [%f]", &ins_dt);
-    // VisionSend();  // 解算完成后发送视觉数据,但是当前的实现不太优雅,后续若添加硬件触发需要重新考虑结构的组织
-    osDelay(1);
-  }
-}
+// __attribute__((noreturn)) void StartINSTASK(void const *argument) {
+//   static float ins_start;
+//   static float ins_dt;
+//   // INS_Init();  // 确保BMI088被正确初始化.
+//   LOGINFO("[freeRTOS] INS Task Start");
+//   for (;;) {
+//     // 1kHz
+//     ins_start = DWT_GetTimeline_ms();
+//     INS_Task();
+//     ins_dt = DWT_GetTimeline_ms() - ins_start;
+//     if (ins_dt > 1) LOGERROR("[freeRTOS] INS Task is being DELAY! dt = [%f]", &ins_dt);
+//     // VisionSend();  // 解算完成后发送视觉数据,但是当前的实现不太优雅,后续若添加硬件触发需要重新考虑结构的组织
+//     osDelay(1);
+//   }
+// }
 
 __attribute__((noreturn)) void StartMOTORTASK(void const *argument) {
   static float motor_dt;
