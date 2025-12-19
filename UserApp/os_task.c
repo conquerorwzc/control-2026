4c98d181916b09c7b5911a515455ef9c76cb5c78
陈孙言 <1970691693@qq.com>
2025-12-19 19:19:52 +0800

    [fix]更改控制频率

--- UserApp/os_task.c
+++ UserApp/os_task.c
@@ -108,8 +108,8 @@ __attribute__((noreturn)) void StartROBOTTASK(void const *argument) {
     RobotTask();
     MotorControlTask();
     robot_dt = DWT_GetTimeline_ms() - robot_start;
-    if (robot_dt > 1) LOGERROR("[freeRTOS] ROBOT core Task is being DELAY! dt = [%f]", &robot_dt);
-    osDelay(1);
+    if (robot_dt > 2) LOGERROR("[freeRTOS] ROBOT core Task is being DELAY! dt = [%f]", &robot_dt);
+    osDelay(2);
   }
 }
 
