05119a6ec156e8861503b0473a6a78aa2c729b2f
吴其华 <16212835+darren-wu@user.noreply.gitee.com>
2025-12-26 21:30:16 +0800

    [feat]并非改好了键鼠，现在按wzc的改法修改

--- UserApp/robot/hero_mecanum/robot.c
+++ UserApp/robot/hero_mecanum/robot.c
@@ -204,11 +204,11 @@ if (gimbal_ctrl_cmd->gimbal_mode == GIMBAL_ON)
   switch (rc_data[TEMP].mouse.press_l % 2)        // 左键发射
   {
   case 0:
-      if(switch_is_up(rc_data[TEMP].rc.switch_left)) {
-        break;
-      }
+      if(!switch_is_up(rc_data[TEMP].rc.switch_left)) {
         shoot_ctrl_cmd->load_mode=LOAD_STOP;
         trigger_time = DWT_GetTimeline_s();
+      }
+      break;
   default:
     switch (rc_data[TEMP].key_count[KEY_PRESS][Key_E] % 2)  // E键设置发射模式
     {
