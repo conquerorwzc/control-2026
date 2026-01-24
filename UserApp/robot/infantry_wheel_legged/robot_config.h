6a419fb172bd66805a5a168b88d692285cb1e218
ShiYu Li <lishiyu3300827985@163.com>
2026-01-24 15:18:29 +0800

    [fix,to]把云台和发射的参数修改了，还没测

--- UserApp/robot/infantry_wheel_legged/robot_config.h
+++ UserApp/robot/infantry_wheel_legged/robot_config.h
@@ -34,8 +34,8 @@
 // 云台参数
 #define YAW_CHASSIS_ALIGN_ECD 655  // 云台和底盘对齐指向相同方向时的电机编码器值,若对云台有机械改动需要修改
 #define PITCH_HORIZON_ECD 3494     // 云台处于水平位置时编码器值,若对云台有机械改动需要修改
-#define PITCH_MAX_ANGLE 26.0f      // 云台竖直方向最大角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
-#define PITCH_MIN_ANGLE -35.0f     // 云台竖直方向最小角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
+#define PITCH_MAX_ANGLE 20.0f      // 云台竖直方向最大角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
+#define PITCH_MIN_ANGLE -20.0f     // 云台竖直方向最小角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
 // 私有宏,自动将编码器转换成角度值
 #define YAW_ALIGN_ANGLE (YAW_CHASSIS_ALIGN_ECD * ECD_ANGLE_COEF_DJI)  // 对齐时的角度,0-360
 #define PTICH_HORIZON_ANGLE (PITCH_HORIZON_ECD * ECD_ANGLE_COEF_DJI)  // pitch水平时电机的角度,0-360
@@ -44,8 +44,8 @@
 #define GYRO2GIMBAL_DIR_ROLL 1   // 陀螺仪数据相较于云台的roll的方向,1为相同,-1为相反
 
 // 发射参数
-#define ONE_BULLET_DELTA_ANGLE (36.0f * 45.0f)  // 发射一发弹丸拨盘转动的距离,由机械设计图纸给出
-#define REDUCTION_RATIO_LOADER 36.0f            // 2006拨盘电机的减速比,英雄需要修改为3508的19.0f
+#define ONE_BULLET_DELTA_ANGLE 36.0f  // 发射一发弹丸拨盘转动的距离,由机械设计图纸给出
+#define REDUCTION_RATIO_LOADER 90.0f            // 2006拨盘电机的减速比,英雄需要修改为3508的19.0f
 #define NUM_PER_CIRCLE 10                       // 拨盘一圈的装载量
 
 // delta_h = 0.380 - 0.112 = 0.268;
@@ -227,22 +227,22 @@ static Gimbal_Init_Config_s gimbal_init_config = {
                 {
                     .angle_PID =
                         {
-                            .Kp = 0.3f,
+                            .Kp = 2.5f,
                             .Ki = 0.0f,
                             .Kd = 0.0f,
-                            .DeadBand = 0.1f,
+                            .DeadBand = 0.01f,
                             .Improve = PID_Trapezoid_Intergral | PID_Integral_Limit | PID_Derivative_On_Measurement,
                             .IntegralLimit = 5.0f,
-                            .MaxOut = 22.0f,
+                            .MaxOut = 25.0f,
                         },
                     .speed_PID =
                         {
-                            .Kp = -6000.0f,
-                            .Ki = -100.0f,
+                            .Kp = -3000.0f,
+                            .Ki = -400.0f,
                             .Kd = 0.0f,
                             .Improve = PID_Trapezoid_Intergral | PID_Integral_Limit | PID_Derivative_On_Measurement,
                             .IntegralLimit = 12000.0f,
-                            .MaxOut = 25000.0f,
+                            .MaxOut = 20000.0f,
                         },
 
                 },
@@ -260,21 +260,22 @@ static Gimbal_Init_Config_s gimbal_init_config = {
                 {
                     .angle_PID =
                         {
-                            .Kp = 1.0f,
+                            .Kp = 2.0f,
                             .Ki = 0.0f,
                             .Kd = 0.0f,
+                            .DeadBand = 0.01f,
                             .Improve = PID_Trapezoid_Intergral | PID_Integral_Limit | PID_Derivative_On_Measurement,
                             .IntegralLimit = 5.0f,
                             .MaxOut = 25.0f,
                         },
                     .speed_PID =
                         {
-                            .Kp = -5000.0f,
-                            .Ki = -200.0f,
+                            .Kp = -3000.0f,
+                            .Ki = -300.0f,
                             .Kd = 0.0f,
                             .Improve = PID_Trapezoid_Intergral | PID_Integral_Limit | PID_Derivative_On_Measurement,
                             .IntegralLimit = 12000.0f,
-                            .MaxOut = 28000.0f,
+                            .MaxOut = 20000.0f,
                         },
                 },
             .motor_type = GM6020,
@@ -287,15 +288,15 @@ static Gimbal_Init_Config_s gimbal_init_config = {
         },
     .imu_init_config = {.flag = 1, .scale = {1.0f, 1.0f, 1.0f}, .Yaw = 0.0f, .Pitch = 0.0f, .Roll = 0.0f}};
 
-#define FRICTION_MOTOR_CONFIG(handle, id, direction) \
+#define FRICTION_MOTOR_CONFIG(handle, id, motor_direction,feedback_motor) \
   ((Motor_Init_Config_s){                            \
       .controller_param_init_config =                \
           {                                          \
               .speed_PID =                           \
                   {                                  \
-                      .Kp = 0.5f,                    \
-                      .Ki = 0.0f,                    \
-                      .Kd = 0.0f,                    \
+                      .Kp = 1.5f,                    \
+                      .Ki = 0.2f,                    \
+                      .Kd = 0.01f,                    \
                       .Improve = PID_Integral_Limit, \
                       .IntegralLimit = 10000.0f,     \
                       .MaxOut = 15000.0f,            \
@@ -307,7 +308,8 @@ static Gimbal_Init_Config_s gimbal_init_config = {
               .speed_feedback_source = MOTOR_FEED,   \
               .outer_loop_type = SPEED_LOOP,         \
               .close_loop_type = SPEED_LOOP,         \
-              .motor_reverse_flag = direction,       \
+.motor_reverse_flag = motor_direction,                   \
+.feedback_reverse_flag = feedback_motor,       \
           },                                         \
       .motor_type = M3508,                           \
       .can_init_config =                             \
@@ -320,14 +322,20 @@ static Gimbal_Init_Config_s gimbal_init_config = {
 static Shoot_Init_Config_s shoot_init_config = {
     .shoot_param =
         {
-            .one_bullet_delta_angle = 45.0f,  // 发射一发弹丸拨盘转动的距离,由机械设计图纸给出
-            .reduction_ratio_loader = 36.0f,  // M2006拨盘电机的减速比
-            .num_per_circle = 8,              // 拨盘一圈的装载量
-            .loader_direction = 1,            // 拨盘旋转方向,1为正向，-1为反向
-            .friction_num = 2,                // 摩擦轮数量
+      .one_bullet_delta_angle = ONE_BULLET_DELTA_ANGLE,          // 发射一发弹丸拨盘转动的距离,由机械设计图纸给出
+       .reduction_ratio_loader = REDUCTION_RATIO_LOADER,          // M2006拨盘电机的减速比
+       .num_per_circle = NUM_PER_CIRCLE,                     // 拨盘一圈的装载量
+       .loader_direction = 1,                    // 拨盘旋转方向,1为正向，-1为反向
+       .friction_num = 2,                        // 摩擦轮数量
+       .friction_speed = 25000.0f,               // 摩擦轮速度
+       .friction_coefficients = {1.0f, -1.0f},  // 摩擦轮速度比例系数
+       .deadtime_burstfire = 300,
+       .deadtime_onebullet = 500,
+       .target_speed = 0.0f,
+       .bullet_speed_adjustment = 10.0f,
         },
-    .friction_motor_config[0] = FRICTION_MOTOR_CONFIG(&hcan2, 1, MOTOR_DIRECTION_NORMAL),
-    .friction_motor_config[1] = FRICTION_MOTOR_CONFIG(&hcan2, 2, MOTOR_DIRECTION_REVERSE),
+  .friction_motor_config[0] = FRICTION_MOTOR_CONFIG(&hcan2, 1, MOTOR_DIRECTION_NORMAL, MOTOR_DIRECTION_NORMAL),
+   .friction_motor_config[1] = FRICTION_MOTOR_CONFIG(&hcan2, 2, MOTOR_DIRECTION_NORMAL, MOTOR_DIRECTION_NORMAL),
 
     .loader_motor_config =
         {
@@ -335,19 +343,19 @@ static Shoot_Init_Config_s shoot_init_config = {
                 {
                     .angle_PID =
                         {
-                            .Kp = 45.0f,
+                            .Kp = 40.0f,
                             .Ki = 0.0f,
                             .Kd = 0.1f,
                             .MaxOut = 30000.0f,
                         },
                     .speed_PID =
                         {
-                            .Kp = 2.0f,
+                            .Kp = 1.0f,
                             .Ki = 0.4f,
                             .Kd = 0.0f,
                             .Improve = PID_Integral_Limit | PID_ErrorHandle,
                             .IntegralLimit = 5000.0f,
-                            .MaxOut = 6000.0f,
+                            .MaxOut = 10000.0f,
                         },
                 },
             .motor_type = M2006,  // 拨盘电机为M2006
