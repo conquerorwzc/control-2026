8af7e06591b7fbb1b2f0d5839b2d19d7357dee4c
Zicheng Wang <2488641698@qq.com>
2026-03-25 21:49:01 +0800

    [feat,run]从裁判系统读取弹速和红蓝方

--- Modules/master_machine/master_process.c
+++ Modules/master_machine/master_process.c
@@ -16,12 +16,13 @@
 #include "srm_protocol.h"
 #include "ins_task.h"
 #include  "HI05.h"
+#include "rm_referee.h"
 #define VISION_USE_VCP
 
 
 #ifdef VISION_USE_VCP
 static DaemonInstance *vision_daemon_instance;
-
+static  referee_info_t* referee_info;
 static  Vision_Receive_s recv_data;//接收数据
 static  Vision_Send_s send_data;//发送数据
 static  INS_t* current_attitude;
@@ -55,8 +56,8 @@ void UpdateGimbalAttitude(Vision_Send_s *vision_send) {
   vision_send->gimbal_send.pitch = current_attitude_Cboard->Pitch;
   vision_send->gimbal_send.roll = current_attitude_Cboard->Roll;
   vision_send->gimbal_send.mode=0;
-  vision_send->gimbal_send.color=0;
-  vision_send->shoot_send.bullet_speed=21;
+  vision_send->gimbal_send.color=referee_info->GameRobotState.robot_id;
+  vision_send->shoot_send.bullet_speed=referee_info->ShootData.initial_speed;
 
 }
 
@@ -169,7 +170,7 @@ Vision_Receive_s *VisionInit(IMU_Init_Config_s* imu_init_config)
         .reload_count = 5, // 50ms
     };
     vision_daemon_instance = DaemonRegister(&daemon_conf);
-
+  referee_info = GetReferee();
     return &recv_data;
 }
 
