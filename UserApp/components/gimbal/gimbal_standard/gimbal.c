c00a1d2df297c08977fcbda56acceedee3fe20d1
ShiYu Li <lishiyu3300827985@163.com>
2026-03-28 17:10:00 +0800

    [pref]加了陀螺仪存零漂，修复了开自瞄云台转180度

--- UserApp/components/gimbal/gimbal_standard/gimbal.c
+++ UserApp/components/gimbal/gimbal_standard/gimbal.c
@@ -20,6 +20,7 @@
 static GimbalInstance* gimbal;
 static Gimbal_Ctrl_Cmd_s* gimbal_ctrl_cmd;  // 声明但不初始化
 static float pitch_feedforward_scale;
+static float last_yaw_cmd=0.0f;
 
 /**
  * @brief pitch重力补偿前馈
@@ -29,6 +30,13 @@ static float pitch_feedforward_scale;
 static float GetPitchGravityFeedforward(void) {
   float pitch_rad = gimbal->gimbal_IMU_data->Pitch * DEGREE_2_RAD;
   return pitch_feedforward_scale * arm_cos_f32(pitch_rad);
+}
+float wrap180(float now, float last)
+{
+  float diff = now - last;
+  diff = fmodf(diff, 360.0f);
+  diff=diff-360.0f*floorf(diff/360.0f+0.5f);
+  return last + diff;
 }
 
 // static BMI088Instance *bmi088; // 云台IMU
@@ -79,11 +87,17 @@ void GimbalTask() {
     // 停止
     DJIMotorStop(gimbal->yaw_motor);
     DJIMotorStop(gimbal->pitch_motor);
+    gimbal_ctrl_cmd->yaw = gimbal->gimbal_IMU_data->YawTotalAngle;
+    gimbal_ctrl_cmd->pitch = gimbal->gimbal_IMU_data->Pitch;
   } else {
     DJIMotorEnable(gimbal->yaw_motor);
     DJIMotorEnable(gimbal->pitch_motor);
+    //pid调参测试用
+    //gimbal_ctrl_cmd->yaw=40*sin(DWT_GetTimeline_s()*2.5f);
+    gimbal_ctrl_cmd->yaw = wrap180(gimbal_ctrl_cmd->yaw, last_yaw_cmd);
     DJIMotorSetPIDRef(gimbal->yaw_motor, gimbal_ctrl_cmd->yaw);  // yaw和pitch会在robot_cmd中处理好多圈和单圈
     DJIMotorSetPIDRef(gimbal->pitch_motor, gimbal_ctrl_cmd->pitch);
     // gimbal->pitch_motor->motor_controller.final_output += GetPitchGravityFeedforward();  // pitch重力补偿前馈
+    last_yaw_cmd = gimbal_ctrl_cmd->yaw;
   }
 }
