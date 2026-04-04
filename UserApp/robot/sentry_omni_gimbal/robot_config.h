bd1bd19ad0309284d4a75132d95d58156c5eb397
Zicheng Wang <2488641698@qq.com>
2026-04-05 01:45:18 +0800

    [fix]步兵结束

--- UserApp/robot/sentry_omni_gimbal/robot_config.h
+++ UserApp/robot/sentry_omni_gimbal/robot_config.h
@@ -165,7 +165,7 @@ static Shoot_Init_Config_s shoot_init_config = {
             .bullet_speed_deadband = 0.2f,//弹速死区，正负deadband。
             .one_barrel_heat_value = 10,//一发弹丸所需热量
             .shooter_barrel_cooling_value = 12,//每秒冷却回复
-            .shooter_barrel_heat_limit = 80,//热量上限
+            .shooter_barrel_heat_limit = 70,//热量上限
         },
     .friction_motor_config[0] = FRICTION_MOTOR_CONFIG(&hcan1, 2, MOTOR_DIRECTION_NORMAL),
     .friction_motor_config[1] = FRICTION_MOTOR_CONFIG(&hcan1, 1, MOTOR_DIRECTION_NORMAL),
